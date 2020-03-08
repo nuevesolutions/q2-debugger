91d66227594524bd11e4defdb523d3bf9e145e0d
Jam Risser <jam@codejam.ninja>
2020-03-08 17:35:40 +0000

    Improved table

--- src/routes/home/containers/RequestsTable.tsx
+++ src/routes/home/containers/RequestsTable.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
-import { TableInspector } from 'react-inspector';
+import { Table, Head, Body, Row, Data } from 'table-inspector';
+import { Inspector } from 'react-inspector';
 import { RequestRow } from '../../../types';
 
 export interface RequestsTableProps {
@@ -7,11 +8,47 @@ export interface RequestsTableProps {
 }
 
 const RequestsTable: FC<RequestsTableProps> = (props: RequestsTableProps) => {
+  function renderRequests(requests: RequestRow[]) {
+    return requests.map((request: RequestRow) => (
+      <Row key={request.status + request.routingKey + request.url}>
+        <Data>{request.status}</Data>
+        <Data>{request.routingKey}</Data>
+        <Data>
+          <Inspector
+            data={
+              typeof request.requestData === 'string'
+                ? JSON.parse(request.requestData)
+                : request.requestData
+            }
+          />
+        </Data>
+        <Data>
+          <Inspector
+            data={
+              typeof request.responseData === 'string'
+                ? JSON.parse(request.responseData)
+                : request.responseData
+            }
+          />
+        </Data>
+        <Data>{request.url}</Data>
+      </Row>
+    ));
+  }
+
   return (
-    <TableInspector
-      data={props.requests}
-      columns={['status', 'routingKey', 'requestData', 'responseData', 'url']}
-    />
+    <Table>
+      <Head>
+        <Row>
+          <Data>status</Data>
+          <Data>routing key</Data>
+          <Data>request data</Data>
+          <Data>response data</Data>
+          <Data>url</Data>
+        </Row>
+      </Head>
+      <Body>{renderRequests(props.requests)}</Body>
+    </Table>
   );
 };
 
